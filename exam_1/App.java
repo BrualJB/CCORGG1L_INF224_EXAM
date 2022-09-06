c4880c3b47454a6426d2e53a918202c0e686a20b
BrualJB <111825090+BrualJB@users.noreply.github.com>
2022-09-06 09:25:22 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Character Class
+        Student Emman = new Student();
+
+        Emman.surname = "Brual";
+        Emman.firstname = "John Emmanuelle";
+        Emman.middleInitial = "R";
+        Emman.dateOfBirth = "09/28/2000";
+        Emman.StudentNumber = 2022-109842;
+        Emman.StudentEmailAddress = "brualjr@students.national-u.edu.ph";
+        Emman.iAmAwesome = true;
+        Emman.sayMystudentnumber();
+        Emman.sayMyStudentEmailAddress();
+        Emman.SayMyIAmAwesome();
+    }
+}
